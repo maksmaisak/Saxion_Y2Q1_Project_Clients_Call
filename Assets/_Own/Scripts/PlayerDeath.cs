0495c85286cc7825ee5473c8dd7bfb735de104e2
CocoDev <bararu.cosmin14@gmail.com>
2018-09-04 19:15:24 +0200

    Create a script which enables player to die and start over again, import some obstacle models and create a new Obstacle tag for collision purposes.

---
+++ Assets/_Own/Scripts/PlayerDeath.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _spawnPoint = Vector3.zero;
+    [SerializeField]
+    private float _minimumPositionY = -15f;
+
+    private Rigidbody _rb = null;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        StartCoroutine(CheckYBoundary());
+    }
+
+    void GameOver()
+    {
+        // Die
+        // Play Sound
+        // Play Animation
+        // Display Resolution Screen
+        ActivateRigidBodyConstraints(false);
+        transform.position = _spawnPoint;
+        ActivateRigidBodyConstraints(true);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Obstacle"))
+            GameOver();
+    }
+
+    void ActivateRigidBodyConstraints(bool enabled)
+    {
+        Debug.Assert(_rb != null);
+
+        _rb.detectCollisions = enabled;
+        _rb.useGravity = enabled;
+        _rb.freezeRotation = !enabled;
+    }
+
+    IEnumerator CheckYBoundary()
+    {
+        while(this.enabled)
+        {
+            if(transform.position.y < _minimumPositionY)
+                GameOver();
+
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
