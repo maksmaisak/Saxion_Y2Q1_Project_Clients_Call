506f531f160926d51c5119b2b3ec9542e2642ba2
Maks <max.maisak@gmail.com>
2018-09-08 00:51:14 +0200

    Cleaned up folder structure. Decoupled inscreasing and displaying the score

---
+++ Assets/_Own/Scripts/BroadcastEvents/ScoreChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreChange : BroadcastEvent<ScoreChange>
+{
+    public readonly int scoreDelta;
+
+    public ScoreChange(int scoreDelta)
+    {
+        this.scoreDelta = scoreDelta;
+    }
+}
