f1fc53a62f14e10a427eed52ee3c9c7976500b16
Maks <max.maisak@gmail.com>
2018-09-08 02:16:32 +0200

    Cleanup

--- Assets/_Own/Scripts/GameplayObjects/JumpPad.cs
+++ Assets/_Own/Scripts/GameplayObjects/JumpPad.cs
@@ -7,8 +7,6 @@ public class JumpPad : GameplayObject
     [SerializeField] Lane targetLane;
     [SerializeField] float playerDetectionRadius = 0.1f;
 
-    /// TODO Maybe just have a destination lane specifiable instead of all that ^. As in "send player to given lane".
-
     // Use this for initialization
     protected override void Start()
     {
@@ -23,42 +21,7 @@ public class JumpPad : GameplayObject
         );
         if (playerRepresentation == null) return;
 
-        Debug.Log("x");
         /// TODO Fix direct dependency.
         playerRepresentation.gameObject.GetComponent<PlayerController>().JumpTo(targetLane);
     }
-
-    /*private void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("Player"))
-            return;
-
-        var controller = other.GetComponent<PlayerController>();
-        Debug.Assert(controller != null);
-
-        if (controller.isJumping)
-            return;
-
-        Lane targetLane = null;
-        Vector3 targetPosition = controller.transform.position + (_isFacingRight ? Vector3.right : Vector3.left * 3.0f);
-
-        if (_isFacingRight)
-        {
-            if (_currentLane.rightNeighbor != null)
-            {
-                targetLane = _currentLane.rightNeighbor;
-                targetPosition = _currentLane.rightNeighbor.GetJumpDestinationFrom(controller.transform.position);
-            }
-        }
-        else
-        {
-            if (_currentLane.leftNeighbor != null)
-            {
-                targetLane = _currentLane.leftNeighbor;
-                targetPosition = _currentLane.leftNeighbor.GetJumpDestinationFrom(controller.transform.position);
-            }
-        }
-
-        controller.JumpTo(targetPosition, targetLane);
-    }*/
 }
