5a36efde0ca0205ebbffa27376df68a88fd4c7b9
CocoDev <bararu.cosmin14@gmail.com>
2018-09-10 17:08:27 +0200

    Add Script for cage collectable

---
+++ Assets/_Own/Scripts/GameplayObjects/Collectables/Cage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cage : Collectable
+{
+    protected override void OnCollected()
+    {
+        new OnCageOpen().PostEvent();
+        // Play Animation.
+    }
+}
