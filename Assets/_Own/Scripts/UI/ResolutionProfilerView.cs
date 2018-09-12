dd4cc840f423ec445f41885344bca07b213f0e8c
Maks <max.maisak@gmail.com>
2018-09-12 02:39:16 +0200

    Added animations to the profiler view

--- Assets/_Own/Scripts/UI/ResolutionProfilerView.cs
+++ Assets/_Own/Scripts/UI/ResolutionProfilerView.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 using UnityEngine.Assertions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
 
 public class ResolutionProfilerView : MyBehaviour, IEventReceiver<OnResolutionScreen>
 {
     [SerializeField] List<TMP_Text> profileTexts = new List<TMP_Text>();
     [SerializeField] List<Image> profileImages = new List<Image>();
+    [SerializeField] float animationDuration = 1f;
+    [SerializeField] float animationInitialDelay = 1f;
 
     // Use this for initialization
     private void Start()
@@ -19,19 +23,26 @@ public class ResolutionProfilerView : MyBehaviour, IEventReceiver<OnResolutionSc
 
     public void On(OnResolutionScreen resolution)
     {
-        var profiles = resolution.profiles;
+        Dictionary<PlayerProfile, int> profiles = resolution.profiles;
 
-        float totalAmount = 0;
-        foreach (var profile in profiles)
-            totalAmount += profile.Value;
+        float totalAmount = profiles.Sum(p => p.Value);
+
+        var seq = DOTween.Sequence();
+        seq.AppendInterval(animationInitialDelay);
 
         // Set fill Amounts for images and text for percentages
         for (int i = 0; i < 4; i++)
         {
-            PlayerProfile playerProfile = (PlayerProfile)i;
-            float profilePercent = totalAmount > 0 ? (float)profiles[playerProfile] / totalAmount : 0.0f;
-            profileImages[i].fillAmount = Mathf.Clamp(profilePercent, 0.01f, 1f);
-            profileTexts[i].text = (Math.Round(profilePercent * 100.0f, 1)) + "%";
+            float profileFraction = Mathf.InverseLerp(0f, totalAmount, profiles[(PlayerProfile)i]);
+            
+            profileTexts[i].text = Math.Round(profileFraction * 100f, 1) + "%";
+
+            var image = profileImages[i];
+            image.fillAmount = 0f;
+            var tween = image
+                .DOFillAmount(Mathf.Clamp(profileFraction, 0.01f, 1f), animationDuration)
+                .SetEase(Ease.InQuad);
+            seq.Join(tween);
         }
     }
 }
