f63b0173d0e7264785e2da2dbbc9e4ce6bd47f43
Maks <max.maisak@gmail.com>
2018-09-17 12:16:37 +0200

    Added the spinning portal thing for portal transitions

---
+++ Assets/_Own/Scripts/Utility/Spinner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] Vector3 rotationPerSecond;
+
+    void Start()
+    {
+        transform
+            .DOLocalRotate(rotationPerSecond, 1f, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Incremental);
+    }
+}
