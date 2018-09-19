ac2fb96e85b9a0b806479fdd262ec93421f63daa
Maks <max.maisak@gmail.com>
2018-09-20 00:22:24 +0200

    Better bird chirp sound

--- Assets/_Own/Scripts/GameplayObjects/Collectables/Collectable.cs
+++ Assets/_Own/Scripts/GameplayObjects/Collectables/Collectable.cs
@@ -5,6 +5,7 @@ public abstract class Collectable : GameplayObject
 {
     [FormerlySerializedAs("playerDetectionRadius")] 
     [SerializeField] float playerDetectionMargin = 0.2f;
+    [Tooltip("Tall collectables will be collected regardless of whether the player is jumping over it or not.")]
     [SerializeField] bool isTall;
 
     protected virtual void FixedUpdate()
