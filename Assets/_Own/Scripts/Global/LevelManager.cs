3fd69cee694b58ac6530d11e02a66a12f4bf0829
Maks <max.maisak@gmail.com>
2018-09-19 13:46:08 +0200

    Made it possible to specify which level is first in the inspector of LevelManager

--- Assets/_Own/Scripts/Global/LevelManager.cs
+++ Assets/_Own/Scripts/Global/LevelManager.cs
@@ -6,14 +6,20 @@ using UnityEngine.SceneManagement;
 /// TODO Make the level sequences editable in the inspector of this.
 public class LevelManager : PersistentSingleton<LevelManager>
 {
+    [Header("Scene")]
+    [SerializeField] string mainMenu = "main_menu";
+    [Header("Story mode")]
+    [SerializeField] string firstLevel = "Tutorial";
+    [Header("Endless mode (TODO)")]
+    
     private readonly Stack<int> previousSceneBuildIndices = new Stack<int>();
-
+        
     private bool isSceneBeingLoaded;
     private bool isPreloadedScreenAwaiting;
 
     public void StartStoryMode()
     {
-        LoadLevel(SceneNames.mainLevelName);
+        LoadLevel(firstLevel);
         new OnGameplaySessionStarted().SetDeliveryType(MessageDeliveryType.Immediate).PostEvent();
     }
     
--- Assets/_Own/Scripts/Utility/SceneNames.cs
+++ Assets/_Own/Scripts/Utility/SceneNames.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
+/// WARNING: THIS IS BEING PHASED OUT
 public static class SceneNames
 {
     // This stuff has to be hardcoded since you can't just assign scenes in the inspector.
     // Still better to keep it centralized anyway. A solution using ScriptableObjects might be
-    // possible, but there's no time
-    //public const string managementName = "management";
-    //public const string managementPath = "Assets/_Own/Scenes/Final/management.unity";
-
+    // possible, but there's no time.
+    
     public const string preload = "__preload";
     public const string mainMenuName = "main_menu";
     public const string mainLevelName = "main";
