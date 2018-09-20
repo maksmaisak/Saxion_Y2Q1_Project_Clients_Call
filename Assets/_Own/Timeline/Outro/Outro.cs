d0495506a3b37d67d0d4040b50c5bc4184bf1983
Maks <max.maisak@gmail.com>
2018-09-20 09:47:01 +0200

    Working on the outro

---
+++ Assets/_Own/Timeline/Outro/Outro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Playables;
+
+public class Outro : MonoBehaviour
+{
+    [SerializeField] PlayableDirector outroDirector;
+
+    private bool wasTriggered;
+
+    void Start()
+    {
+        if (!outroDirector) outroDirector = GetComponent<PlayableDirector>();
+        Assert.IsNotNull(outroDirector);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (wasTriggered) return;
+        if (!enabled) return;
+        if (!other.CompareTag("Player")) return;
+        
+        outroDirector.Play();
+        this.Delay((float)outroDirector.duration, EndGame);
+
+        wasTriggered = true;
+    }
+
+    private void EndGame()
+    {
+        new OnGameOver().PostEvent();
+    }
+}
